7d8e1386804e428f5d3c265fbf13db39601e29af
Mondei1 <emailchecker000@gmail.com>
2019-03-12 17:47:50 +0100

    + Implemented dummy documents in code.

--- backend/src/dummyData.ts
+++ backend/src/dummyData.ts
@@ -6,16 +6,18 @@ import { createPasswordHash } from "./libs/createPasswordHash";
 
 
 export async function insertDummyData() {
+    // Declare dummy users
     const dummyUsers: Array<any> = [
         {
-            username: "Mondei1",
-            password: "pass"
+            username: "test",
+            password: "test"
         },
         {
-            username: "spYro",
-            password: "PASS"
+            username: "test2",
+            password: "test2"
         }
     ]
+
     // Write dummy users into database
     for(let dummyUser of dummyUsers) {
         const salt: string = createRandomString(16);
@@ -27,57 +29,160 @@ export async function insertDummyData() {
             salt: salt
         }).save();
     }
-    let user: User = await User.findOne({where: {username: "spYro"}});
-    // 1. save document
-    let highestDoc = await Document.findOne({
-        select: ["primaryNumber"],
-        order: { primaryNumber: "DESC" }
-    })
-    let doc1 = new Document();
-    if(highestDoc == null) {
-        doc1.primaryNumber = 1;
-    } else {
-        doc1.primaryNumber = highestDoc.primaryNumber + 1;
-    }
-    doc1.secondaryNumber = 1;
-    doc1.user = user;
-    doc1.fileExtension = "png";
-    doc1.title = "Rechnung TUI Flitterwochen 2019";
-    doc1.note = "Rechnung von den Flitterwochen zu den Malediven (Fushifaru) Ende 2019";
+    let test: User = await User.findOne({where: {username: "test"}});
+    let test2: User = await User.findOne({where: {username: "test2"}});
 
-    let tagRechnung = await Tag.create({
-        name: "Rechnung",
+    // Create tags
+    const tagInvoice = await Tag.create({
+        name: "Invoice",
+        colorBackground: "#f1c40f",
+        colorForeground: '#ffffff',
+        user: test
+    }).save();
+    const tagTravel = await Tag.create({
+        name: "Travel",
+        colorBackground: "#2ecc71",
+        colorForeground: '#ffffff',
+        user: test
+    }).save();
+    const tagWarning = await Tag.create({
+        name: "Warning",
+        colorBackground: "#e74c3c",
+        colorForeground: "#ffffff",
+        user: test
+    }).save();
+    const tagMisc = await Tag.create({
+        name: "Misc",
         colorBackground: "#1c1c1c",
-        colorForeground: '#ffffff',
-        user: user
-    }).save();
-    let tagReise = await Tag.create({
-        name: "Reise",
-        colorBackground: "#FFC107",
-        colorForeground: '#ffffff',
-        user: user
-    }).save();
-    let tagMahnung = await Tag.create({
-        name: "Mahnung",
-        user: user
+        colorForeground: "#ffffff",
+        user: test
     }).save();
 
-    doc1.tags = new Array<Tag>();
-    doc1.tags.push(tagRechnung, tagReise);
+    const documents = [
+        {
+            primaryNumber: 1,
+            secondaryNumber: 0,
+            title: "Sample document 1 - Watermarked",
+            note: "What the title says",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagMisc]
+        },
+        {
+            primaryNumber: 2,
+            secondaryNumber: 0,
+            title: "Sample document 2",
+            note: "This is cool text",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagWarning, tagInvoice]
+        },
+        {
+            primaryNumber: 3,
+            secondaryNumber: 0,
+            title: "Sample document 3",
+            note: "$4.500 :(",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagTravel, tagInvoice]
+        },
+        {
+            primaryNumber: 4,
+            secondaryNumber: 0,
+            title: "Sample document 4 awww",
+            note: "Awwww",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagMisc]
+        },
+        {
+            primaryNumber: 5,
+            secondaryNumber: 0,
+            title: "Sample document 5",
+            note: "Did you????",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagInvoice, tagWarning]
+        },
+        {
+            primaryNumber: 5,
+            secondaryNumber: 1,
+            title: "Sample document 6 - Magic",
+            note: "Right?",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagMisc, tagWarning]
+        },
+        {
+            primaryNumber: 5,
+            secondaryNumber: 2,
+            title: "Sample document 7",
+            note: "More magic",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagInvoice, tagMisc, tagTravel, tagWarning]
+        },
+        {
+            primaryNumber: 6,
+            secondaryNumber: 0,
+            title: "Can you OCR?",
+            note: "[Fill in here a good note]",
+            fileExtension: 'pdf',
+            user: test2,
+            tags: [tagInvoice]
+        },
+        {
+            primaryNumber: 7,
+            secondaryNumber: 0,
+            title: "Sample document 9 - Base64",
+            note: "Bipp, boop, bipp",
+            fileExtension: 'pdf',
+            user: test2,
+            tags: [tagMisc, tagWarning]
+        },
+        {
+            primaryNumber: 8,
+            secondaryNumber: 0,
+            title: "Sample document 10",
+            note: "Funny joke",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagTravel]
+        },
+        {
+            primaryNumber: 9,
+            secondaryNumber: 0,
+            title: "Sample document 11",
+            note: "ftw!!!!!!",
+            fileExtension: 'pdf',
+            user: test,
+            tags: [tagInvoice]
+        },
+        {
+            primaryNumber: 10,
+            secondaryNumber: 0,
+            title: "Sample document 12 - pnnnnnnnng",
+            note: "PDF is boring, now PNG.",
+            fileExtension: 'png',
+            user: test,
+            tags: [tagMisc, tagInvoice]
+        },
+        {
+            primaryNumber: 11,
+            secondaryNumber: 0,
+            title: "Sample document 13",
+            note: "Be brave.",
+            fileExtension: 'png',
+            user: test2,
+            tags: [tagInvoice]
+        }
+    ]
 
-    doc1.mimeType = "image/png";
-    await doc1.save();
-
-    // 2. Document speichern
-    let doc2 = new Document();
-    doc2.primaryNumber = doc1.primaryNumber;
-    doc2.secondaryNumber = doc1.secondaryNumber + 1;
-    doc2.fileExtension = "png";
-    doc2.title = "Mahnung TUI";
-    doc2.note = "Mahnung von TUI von den Flitterwochen zu den Malediven (Fushifaru) Ende 2019";
-    doc2.user = user;
-    doc2.tags = new Array<Tag>();
-    doc2.tags.push(tagRechnung, tagMahnung);
-    doc2.mimeType = "image/png";
-    await doc2.save();
+    // Save documents
+    console.log("Begin to save all dummy documents ...")
+    for(let i = 0; i < documents.length; i++) {
+        const document = documents[i];
+        await Document.create(document).save();
+        console.log(`Document ${i} of ${documents.length} has been saved.`)
+    }
 }
