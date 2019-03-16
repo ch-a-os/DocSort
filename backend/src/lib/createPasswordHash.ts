b9275f186ab40de906b458bfb4cf403e10169a51
720degreeLotus <720degreelotus@gmail.com>
2019-03-16 12:48:39 +0100

    changed hashrounds to 2 for development, will be changed again when releasing

--- backend/src/lib/createPasswordHash.ts
+++ backend/src/lib/createPasswordHash.ts
@@ -2,13 +2,12 @@ import * as crypto from 'crypto';
 
 /**
  * This function creates a hash of a given password and salt.
- * This takes about 1.3sek to complete because of 35 hash iterations. 
  * @param password Password to hash
  * @param salt String that get's appended at password
  */
 export async function createPasswordHash(password: string, salt: string): Promise<string> {
     return new Promise<string>(async (resolve, reject) => {
-        const hashRounds = 100;
+        const hashRounds = 2;
         let computedHash = await scrypt(password, salt);    // First round
         for(let x = 0; x < hashRounds; x++) {
             try {
