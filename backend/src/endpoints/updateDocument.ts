023380311e691243666b595c8e61581fbfaad7f7
Mondei1 <emailchecker000@gmail.com>
2019-03-21 17:19:11 +0100

    + Added patch method for documents

---
+++ backend/src/endpoints/updateDocument.ts
@@ -0,0 +1,26 @@
+import { IUser } from "../models/user/user.interface";
+import { User } from "../models/user/user.model";
+import { getUserIDFromJWT } from "../lib/getUserIDFromJWT";
+import { IDocument } from "../models/document/document.interface";
+import { Document } from "../models/document/document.model";
+
+export default async function updateDocument(req, res) {
+    const user: IUser = await User.findById(getUserIDFromJWT(req.headers.token)).exec();
+    const changedDoc: IDocument = req.body;
+    
+    // Check If body exist
+    if(changedDoc == null) {
+        res.status(400).send();
+        return;
+    }
+
+    // Check If user was found
+    if(user == null) {
+        res.status(401).send();
+        return;
+    }
+
+    await Document.update({_id: changedDoc._id, user_R: user}, changedDoc).exec();
+
+    res.status(200).send();
+}
--- backend/src/lib/registerExpressRoutes.ts
+++ backend/src/lib/registerExpressRoutes.ts
@@ -8,6 +8,7 @@ import createTag from "../endpoints/createTag";
 import updateTag from "../endpoints/updateTag";
 import deleteTag from "../endpoints/deleteTag";
 import { validateJWT } from "./validateJWT";
+import updateDocument from "../endpoints/updateDocument";
 
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
@@ -15,9 +16,12 @@ const upload = multer({ storage: multer.memoryStorage() });
 export function registerExpressRoutes(app) {
     app.get('/login', login);
     
+    /* DOCUMENT */
     app.get('/searchDocuments', validateJWT, searchDocuments);
     app.get('/getDocument', validateJWT, getDocument);
-    app.get('/getDocumentFile/:docID', validateJWT, getDocumentFile);
+    app.patch('/updateDocument', validateJWT, updateDocument);
+    //app.delete('/deleteDocument', validateJWT,);
+    app.get('/getDocumentFile/:docID', validateJWT, getDocumentFile);   // To get the binary file
     
 	/* TAG */
     app.get('/getAllTags', validateJWT, getAllTags);
