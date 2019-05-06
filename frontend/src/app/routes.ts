f6b9b8ac1bc120873cb7de2a74f72b1ca6a824ec
Nicolas <klier.nicolas@protonmail.com>
2019-05-06 17:59:02 +0200

    Removed comment of the guard service

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 //import { LoginComponent } from './login/login.component';
-//import { GuardService } from './guard.service';
 //import { AppComponent } from './app.component';
 import { PageHomeComponent } from './page-home/page-home.component';
 import { PageUploadSingleFileComponent } from './page-upload-single-file/page-upload-single-file.component';
